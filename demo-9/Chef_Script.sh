e52a16146c32cf40597a19b5354bca86f41747f7
Rajbir Singh Sood <rajbirsingh@Virtusa.com>
2018-03-14 01:45:30 +0530

    modified script

--- demo-9/Chef_Script.sh
+++ demo-9/Chef_Script.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-yum update
-yum install git
-yum install dpkg
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install dpkg
 
-chef_repo="/home/ubuntu/chef-repo"
-
+chef_repo="$HOME/chef-repo"
+cd $HOME
 echo $chef_repo
 
 test -d "/opt/chef" || \
 (
-  # Install Chef ("omnibus")
+  # Install Chef solo("omnibus")
     #
    echo '-- Installing Chef -----------';
   curl -L http://www.opscode.com/chef/install.sh | bash
@@ -26,6 +26,9 @@ wget https://packages.chef.io/files/stable/chefdk/2.4.17/ubuntu/16.04/chefdk_2.4
 sudo dpkg -i *.deb
 )
 
+git clone https://github.com/rajbirsood/chef-repo.git
+
+: '
 if [ -d "$chef_repo" ];
   then
    echo "$chef_repo exist."
@@ -35,8 +38,7 @@ if [ -d "$chef_repo" ];
    echo "$chef_repo does not exist" >&2dministrator
    git clone https://github.com/rajbirsood/chef-repo.git
 fi
-
-
+'
 cd $chef_repo
 
 sudo chef-solo -c .chef/solo.rb -j .chef/solo.json
